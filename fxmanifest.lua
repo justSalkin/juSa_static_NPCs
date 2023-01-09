ae9efe0c8ee344b44bc1d108eec30234d2f523f1
Niklas B <68842909+justSalkin@users.noreply.github.com>
2023-01-09 12:02:52 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'justSalkin'
 description 'Script for static NPCs'
-version '1.0.0'
+version '1.2.0'
 
 
 client_script {
